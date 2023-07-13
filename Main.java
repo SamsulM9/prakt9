c068acd5f03a3a65b46c08c5c7da47333ca52787
SamsulM9 <123233104+SamsulM9@users.noreply.github.com>
2023-07-14 06:41:34 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
     
     public static void main(String[] args) {
-        
-        Buku buku1 = new Buku("B001", "Pemrograman Java");
+       
+        new Buku("B001", "Pemrograman Java");
         Buku buku2 = new Buku("B002", "Pemrograman C++");
-        BukuLuar buku3 = new BukuLuar("B003", "Harry Potter");
+        new BukuLuar("B003", "Harry Potter");
 
         Petugas petugas1 = new Petugas("Alex", "P001");
         Peminjam peminjam1 = new Mahasiswa();
@@ -12,19 +12,31 @@ public class Main {
         peminjam1.setKodePeminjam("M001");
 
         peminjam1.doLogin();
-       // peminjam1.doLogout();
 
     try{
         Peminjaman peminjaman1 = new Peminjaman(peminjam1, petugas1, buku2);
+        Denda denda1 = new Denda(peminjaman1, 7);
 
         System.out.println("Nama Peminjam: " + peminjaman1.getPeminjam().getNamaPeminjam());
         System.out.println("Kode Peminjam " + peminjaman1.getPeminjam().getKodePeminjam());
         System.out.println("Nama Petugas: " + peminjaman1.getPetugas().getNamaPetugas());
         System.out.println("Nama Buku: " + peminjaman1.getBuku().getNamaBuku());
+
+        System.out.println("Tagihan denda: " + denda1.getTagihan());
+
+        System.out.println("=== Denda dihapuskan ! ===");
+        peminjam1.addAntiDenda();
+        System.out.println("Tagihan denda: " + denda1.getTagihan());
+
+        System.out.println("=== Denda ditambahkan ! ===");
+        peminjam1.removeAntiDenda();
+        System.out.println("Tagihan denda: " + denda1.getTagihan());
+
     } catch (Exception e) {
         System.out.println("Terjadi kesalahan: " + e.getMessage());
     } finally {
         System.out.println("Program selesai");
     }
+    peminjam1.doLogout();
     }
 }
